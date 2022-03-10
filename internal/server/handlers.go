e0308dac69f55f242baf8ed56b12fa5ff7c6d1a4
lebensborned <lebensborn.dev@gmail.com>
2022-03-10 08:10:05 +0300

    add simple response json writer

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,24 +1,35 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
+type Response struct {
+	Msg string `json:"msg"`
+}
+
+func WriteStringResponse(s string) []byte {
+	resp := Response{Msg: s}
+	bytes, _ := json.Marshal(resp)
+	return bytes
+}
+
 // Handler for /order/{id}
 func (s *Server) GetOrderById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	if _, ok := vars["id"]; !ok {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Bad request"))
+		w.Write(WriteStringResponse("Bad request"))
 		return
 	}
 	order, err := s.repo.FindById(vars["id"])
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Order not found"))
+		w.Write(WriteStringResponse("Order not found"))
 		return
 	}
 	tmp, err := template.ParseFiles("ui/templates/order.html")
